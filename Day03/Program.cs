0feaf66530c22e782a04947a71cdba75cfadbdb5
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-09 12:05:08 -0800

    Complete Day 3

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -2,20 +2,29 @@
 
 internal static class Day03
 {
-    private const string InputFileName = "./Inputs/FullInput.txt";
-    
+    private const string InputFileName = "./Inputs/PuzzleInput.txt";
+
     private static void Main(string[] args)
     {
         var text = File.ReadAllText(InputFileName);
-        
+        var nVisited = CountHousesVisited(ref text);
+        var nVisitedWithRoboSanta = CountHousesVisited(ref text, 2);
+        Console.WriteLine($"Houses visited with just one Santa: {nVisited}");
+        Console.WriteLine($"Houses visited with Santa and Robo-Santa: {nVisitedWithRoboSanta}");
+    }
+    
+    private static int CountHousesVisited(ref string text, int nSantas=1)
+    {
         var visits = new Dictionary<(int x, int y), int>
         {
-            [(0, 0)] = 1
+            [(0, 0)] = nSantas
         };
-        (int x, int y) pos = (0, 0);
+        var santaPositions = new (int x, int y)[nSantas];
+        var currentSanta = 0;
         
         foreach (var c in text)
         {
+            ref var pos = ref santaPositions[currentSanta];
             switch (c)
             {
                 case '<':
@@ -33,7 +42,9 @@ internal static class Day03
             }
 
             visits[pos] = visits.GetValueOrDefault(pos, 0) + 1;
+            currentSanta = (currentSanta + 1) % nSantas;
         }
-        Console.WriteLine($"Houses visited: {visits.Keys.Count}");
+
+        return visits.Keys.Count;
     }
 }
