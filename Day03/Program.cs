b7cf2c92bf0ff2540d06e5d00d426fe1043c6ff8
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-08 18:11:26 -0800

    Day 3 part 1

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -2,8 +2,38 @@
 
 internal static class Day03
 {
+    private const string InputFileName = "./Inputs/FullInput.txt";
+    
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, world!");
+        var text = File.ReadAllText(InputFileName);
+        
+        var visits = new Dictionary<(int x, int y), int>
+        {
+            [(0, 0)] = 1
+        };
+        (int x, int y) pos = (0, 0);
+        
+        foreach (var c in text)
+        {
+            switch (c)
+            {
+                case '<':
+                    pos.x -= 1;
+                    break;
+                case '^':
+                    pos.y += 1;
+                    break;
+                case '>':
+                    pos.x += 1;
+                    break;
+                case 'v':
+                    pos.y -= 1;
+                    break;
+            }
+
+            visits[pos] = visits.GetValueOrDefault(pos, 0) + 1;
+        }
+        Console.WriteLine($"Houses visited: {visits.Keys.Count}");
     }
 }
