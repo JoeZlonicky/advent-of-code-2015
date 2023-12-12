4b35f98830cfab20fe8d680c1fabf73793e8039a
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-11 23:33:08 -0800

    Refactor day 4

--- Day04/Day04.cs
+++ Day04/Day04.cs
@@ -1,4 +1,6 @@
-﻿namespace Day04;
+﻿using System.Diagnostics;
+
+namespace Day04;
 
 
 internal static class Day04
@@ -6,24 +8,24 @@ internal static class Day04
     private const string StartString = "yzbqklnj";
     internal static void Main()
     {
-        int startsWithFiveZeros = CalcFirstIntegerPostfixHash(StartString, s => StartsWithNZeroes(s, 5));
-        Console.WriteLine($"Answer for 5 zeros: {startsWithFiveZeros}");
+        int startsWithFiveZeros = CalcFirstHash(StartString, s => StartsWithNZeroes(s, 5));
+        Console.WriteLine($"First hash with 5 zeros: {startsWithFiveZeros}");
         
-        int startsWithSixZeros = CalcFirstIntegerPostfixHash(StartString, s => StartsWithNZeroes(s, 6));
-        Console.WriteLine($"Answer for 6 zeros: {startsWithSixZeros}");
+        int startsWithSixZeros = CalcFirstHash(StartString, s => StartsWithNZeroes(s, 6));
+        Console.WriteLine($"First hash with 6 zeros: {startsWithSixZeros}");
     }
 
-    private static int CalcFirstIntegerPostfixHash(string prefix, Func<string, bool> predicate)
+    private static int CalcFirstHash(string inputPrefix, Func<string, bool> resultPredicate)
     {
         using var md5 = System.Security.Cryptography.MD5.Create();
         for (int n = 0; ; ++n)
         {
-            string testString = prefix + n;
+            string testString = inputPrefix + n;
             byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(testString);
             byte[] hashBytes = md5.ComputeHash(inputBytes);
 
             string hash = Convert.ToHexString(hashBytes);
-            if (!predicate(hash)) continue;
+            if (!resultPredicate(hash)) continue;
 
             return n;
         }
@@ -31,6 +33,7 @@ internal static class Day04
 
     private static bool StartsWithNZeroes(string s, int nZeroes)
     {
+        Debug.Assert(nZeroes >= 0);
         if (s.Length < nZeroes) return false;
         
         for (int i = 0; i < nZeroes; ++i)
