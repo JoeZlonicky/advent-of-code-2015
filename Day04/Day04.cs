cee2cddad9973e78322ac2ff13ae1cbaf777515e
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-12 17:37:26 -0800

    Update README.md

---
+++ Day04/Day04.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+namespace Day04;
+
+
+internal static class Day04
+{
+    private const string InputFileName = "./Inputs/Puzzle.txt";
+    internal static void Main()
+    {
+        string startString = File.ReadAllText(InputFileName);
+        int startsWithFiveZeros = CalcFirstHash(startString, s => StartsWithNZeroes(s, 5));
+        Console.WriteLine($"First hash with 5 zeros: {startsWithFiveZeros}");
+        
+        int startsWithSixZeros = CalcFirstHash(startString, s => StartsWithNZeroes(s, 6));
+        Console.WriteLine($"First hash with 6 zeros: {startsWithSixZeros}");
+    }
+
+    private static int CalcFirstHash(string inputPrefix, Func<string, bool> resultPredicate)
+    {
+        using var md5 = System.Security.Cryptography.MD5.Create();
+        for (int n = 0; ; ++n)
+        {
+            string testString = inputPrefix + n;
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(testString);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            string hash = Convert.ToHexString(hashBytes);
+            if (!resultPredicate(hash)) continue;
+
+            return n;
+        }
+    }
+
+    private static bool StartsWithNZeroes(string s, int nZeroes)
+    {
+        Debug.Assert(nZeroes >= 0);
+        if (s.Length < nZeroes) return false;
+        
+        for (int i = 0; i < nZeroes; ++i)
+        {
+            if (s[i] != '0') return false;
+        }
+        return true;
+    } 
+}
---
+++ Day05/Day05.cs
@@ -0,0 +1,68 @@
+﻿namespace Day05;
+
+
+internal static class Day05
+{
+    private const string InputFileName = "Inputs/Puzzle.txt";
+    private const string Vowels = "aeiou";
+
+    private static void Main()
+    {
+        string[] lines = File.ReadAllLines(InputFileName);
+        int nValid = lines.Aggregate(0, (n, s) =>
+        {
+            bool isValid = ContainsAtLeastNVowels(s, 3);
+            isValid &= ContainsAtLeastNRepeatedLetters(s, 2);
+            isValid &= DoesNotContainAnyOf(s, "ab", "cd", "pq", "xy");
+            return isValid ? n + 1 : n;
+        });
+        Console.WriteLine($"N valid strings: {nValid}");
+    }
+
+    private static bool ContainsAtLeastNVowels(string s, int nVowels)
+    {
+        if (nVowels == 0) return true;
+        
+        int vowelCount = 0;
+        foreach (char c in s)
+        {
+            if (Vowels.Contains(c)) ++vowelCount;
+            if (vowelCount >= nVowels) return true;
+        }
+
+        return false;
+    }
+
+    private static bool ContainsAtLeastNRepeatedLetters(string s, int nRepeated)
+    {
+        if (nRepeated == 0) return true;
+
+        int repeatCount = 0;
+        char repeatChar = '\0';
+        foreach (char c in s)
+        {
+            if (repeatChar == '\0')
+            {
+                repeatChar = c;
+                repeatCount = 1;
+                if (repeatCount == nRepeated) return true;
+            }
+            else if (c == repeatChar) {
+                ++repeatCount;
+                if (repeatCount == nRepeated) return true;
+            }
+            else
+            {
+                repeatCount = 1;
+                repeatChar = c;
+            }
+        }
+
+        return false;
+    }
+
+    private static bool DoesNotContainAnyOf(string s, params string[] invalids)
+    {
+        return invalids.All(invalid => !s.Contains(invalid));
+    }
+}
