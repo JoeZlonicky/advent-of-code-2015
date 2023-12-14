15a56ce656ca3cfd74b100f864f0a50fbe89e7a5
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-13 16:18:13 -0800

    Day 6 part 1

---
+++ Day06/Day06.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day06;
+
+internal static class Day06
+{
+    private const string InputFileName = "Inputs/Puzzle.txt";
+    private const string InputLineRegex = @"([a-zA-Z ]*)(\d*),(\d*)(?:[a-zA-Z ]*)(\d*),(\d*)";
+    
+    private const int GridWidth = 1000;
+    private const int GridHeight = 1000;
+    
+    private static void Main()
+    {
+        var grid = new LightGrid(GridWidth, GridHeight);
+        var re = new Regex(InputLineRegex, RegexOptions.IgnoreCase);
+        string[] inputsLines = File.ReadAllLines(InputFileName);
+        
+        foreach (string line in inputsLines)
+        {
+            var match = re.Match(line);
+            if (!match.Success) continue;
+            
+            string instruction = match.Groups[1].Value.Trim();
+            int x1 = int.Parse(match.Groups[2].Value);
+            int y1 = int.Parse(match.Groups[3].Value);
+            int x2 = int.Parse(match.Groups[4].Value);
+            int y2 = int.Parse(match.Groups[5].Value);
+            switch (instruction)
+            {
+                case "toggle":
+                    grid.Toggle(x1, y1, x2, y2);
+                    break;
+                case "turn off":
+                    grid.TurnOff(x1, y1, x2, y2);
+                    break;
+                case "turn on":
+                    grid.TurnOn(x1, y1, x2, y2);
+                    break;
+            }
+        }
+        
+        Console.WriteLine($"Lights on: {grid.GetNTurnedOn()}");
+    }
+}
---
+++ Day06/LightGrid.cs
@@ -0,0 +1,58 @@
+﻿namespace Day06;
+
+internal class LightGrid
+{
+    private int Width { get; set; }
+    private int Height { get; set; }
+    
+    private bool[,] Rows { get; set; }
+    
+    internal LightGrid(int width, int height)
+    {
+        Width = width;
+        Height = height;
+
+        Rows = new bool[width, height];
+    }
+
+    internal int GetNTurnedOn()
+    {
+        return Rows.Cast<bool>().Count(b => b);
+    }
+    
+    // Turns on a rectangle of lights from (x1, y1) to (x2, y2) inclusive
+    internal void TurnOn(int x1, int y1, int x2, int y2)
+    {
+        for (int x = x1; x <= x2; ++x)
+        {
+            for (int y = y1; y <= y2; ++y)
+            {
+                Rows[x, y] = true;
+            }
+        }
+    }
+
+    // Turns off a rectangle of lights from (x1, y1) to (x2, y2) inclusive
+    internal void TurnOff(int x1, int y1, int x2, int y2)
+    {
+        for (int x = x1; x <= x2; ++x)
+        {
+            for (int y = y1; y <= y2; ++y)
+            {
+                Rows[x, y] = false;
+            }
+        }
+    }
+
+    // Toggles a rectangle of lights from (x1, y1) to (x2, y2) inclusive
+    internal void Toggle(int x1, int y1, int x2, int y2)
+    {
+        for (int x = x1; x <= x2; ++x)
+        {
+            for (int y = y1; y <= y2; ++y)
+            {
+                Rows[x, y] = !Rows[x, y];
+            }
+        }
+    }
+}
