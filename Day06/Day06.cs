b4443e939f4d4a8f38e4880dad2c12fdfb8fc94e
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-14 08:32:25 -0800

    Day 6 part 2

--- Day06/Day06.cs
+++ Day06/Day06.cs
@@ -9,14 +9,26 @@ internal static class Day06
     
     private const int GridWidth = 1000;
     private const int GridHeight = 1000;
+
+    private delegate void GridInstructionHandler(LightGrid grid, string instruction, int x1, int y1, int x2, int y2);
     
     private static void Main()
     {
-        var grid = new LightGrid(GridWidth, GridHeight);
-        var re = new Regex(InputLineRegex, RegexOptions.IgnoreCase);
         string[] inputsLines = File.ReadAllLines(InputFileName);
+        var grid = new LightGrid(GridWidth, GridHeight);
+        PerformInstructionsOnGrid(grid, inputsLines, PartOneInstructions);
+        Console.WriteLine($"Lights on: {grid.GetNTurnedOn()}");
         
-        foreach (string line in inputsLines)
+        grid.Reset();
+        PerformInstructionsOnGrid(grid, inputsLines, PartTwoInstructions);
+        Console.WriteLine($"---\nTotal value of lights: {grid.SumLights()}");
+    }
+
+    private static void PerformInstructionsOnGrid(LightGrid grid, IEnumerable<string> instructions, GridInstructionHandler handler)
+    {
+        var re = new Regex(InputLineRegex, RegexOptions.IgnoreCase);
+        
+        foreach (string line in instructions)
         {
             var match = re.Match(line);
             if (!match.Success) continue;
@@ -26,20 +38,39 @@ internal static class Day06
             int y1 = int.Parse(match.Groups[3].Value);
             int x2 = int.Parse(match.Groups[4].Value);
             int y2 = int.Parse(match.Groups[5].Value);
-            switch (instruction)
-            {
-                case "toggle":
-                    grid.Toggle(x1, y1, x2, y2);
-                    break;
-                case "turn off":
-                    grid.TurnOff(x1, y1, x2, y2);
-                    break;
-                case "turn on":
-                    grid.TurnOn(x1, y1, x2, y2);
-                    break;
-            }
+
+            handler(grid, instruction, x1, y1, x2, y2);
         }
-        
-        Console.WriteLine($"Lights on: {grid.GetNTurnedOn()}");
+    }
+    private static void PartOneInstructions(LightGrid grid, string instruction, int x1, int y1, int x2, int y2)
+    {
+        switch (instruction)
+        {
+            case "toggle":
+                grid.Toggle(x1, y1, x2, y2);
+                break;
+            case "turn off":
+                grid.TurnOff(x1, y1, x2, y2);
+                break;
+            case "turn on":
+                grid.TurnOn(x1, y1, x2, y2);
+                break;
+        }
+    }
+    
+    private static void PartTwoInstructions(LightGrid grid, string instruction, int x1, int y1, int x2, int y2)
+    {
+        switch (instruction)
+        {
+            case "toggle":
+                grid.Modify(x1, y1, x2, y2, 2);
+                break;
+            case "turn off":
+                grid.Modify(x1, y1, x2, y2, -1);
+                break;
+            case "turn on":
+                grid.Modify(x1, y1, x2, y2, 1);
+                break;
+        }
     }
 }
