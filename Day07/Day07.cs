a88fc65ac75219c4d66f646d8b76c00bbd6cb979
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-15 12:39:43 -0800

    Start day 7

---
+++ Day07/Day07.cs
@@ -0,0 +1,12 @@
+﻿namespace Day07;
+
+internal static class Day07
+{
+    private const string InputFileName = "Inputs/Puzzle.txt";
+    // (LHS?)(AND|OR|LSHIFT|RSHIFT|NOT)?(RHS?)->(OUTPUT)
+    private const string CommandPattern = @"([a-z\d]*)(?: *)(AND|OR|RSHIFT|LSHIFT|NOT)?(?: *)([a-z\d]*)(?: *)->(?: *)([a-z]*)";
+    private static void Main()
+    {
+        
+    }
+}
---
+++ Day07/Emulator.cs
@@ -0,0 +1,36 @@
+﻿namespace Day07;
+
+public class Emulator
+{
+    private Dictionary<string, int> variables;
+
+    internal Emulator()
+    {
+        variables = new Dictionary<string, int>();
+    }
+
+    internal void ExecuteCommand(string lhs, string operation, string rhs, string output)
+    {
+        
+    }
+
+    internal void ParseMoveCommand(string lhs, string output)
+    {
+        
+    }
+
+    internal void MoveCommand(int lhs, string output)
+    {
+        
+    }
+
+    internal void AndCommand(int lhs, int rhs)
+    {
+        
+    }
+
+    internal void ParseAndCommand(string lhs, string rhs)
+    {
+        
+    }
+}
