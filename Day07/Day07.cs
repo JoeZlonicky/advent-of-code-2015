3cb8febb86776e1095e02fedb63c24cd4eb63a9c
Joe Zlonicky <joe.zlonicky@gmail.com>
2023-12-15 16:10:48 -0800

    Still making progress on day 7

--- Day07/Day07.cs
+++ Day07/Day07.cs
@@ -1,12 +1,36 @@
-﻿namespace Day07;
+﻿using System.Text.RegularExpressions;
+
+namespace Day07;
 
 internal static class Day07
 {
-    private const string InputFileName = "Inputs/Puzzle.txt";
+    private const string InputFileName = "Inputs/Example.txt";
     // (LHS?)(AND|OR|LSHIFT|RSHIFT|NOT)?(RHS?)->(OUTPUT)
     private const string CommandPattern = @"([a-z\d]*)(?: *)(AND|OR|RSHIFT|LSHIFT|NOT)?(?: *)([a-z\d]*)(?: *)->(?: *)([a-z]*)";
     private static void Main()
     {
+        string[] lines = File.ReadAllLines(InputFileName);
+        var re = new Regex(CommandPattern, RegexOptions.IgnoreCase);
+        var emulator = new Emulator();
         
+        foreach (string line in lines)
+        {
+            var match = re.Match(line);
+            if (!match.Success) continue;
+            
+            string leftOperand = match.Groups[1].Value.Trim();
+            string operation = match.Groups[2].Value.Trim();
+            string rightOperand = match.Groups[3].Value.Trim();
+            string output = match.Groups[4].Value.Trim();
+            if (leftOperand == "NOT")
+            {
+                operation = leftOperand;
+                leftOperand = "";
+            }
+            
+            emulator.ExecuteCommand(leftOperand, operation, rightOperand, output);
+        }
+        
+        Console.WriteLine(emulator.GetVariableValue("a"));
     }
 }
--- Day07/Emulator.cs
+++ Day07/Emulator.cs
@@ -2,6 +2,8 @@
 
 public class Emulator
 {
+    private static int _maxValue = 65535;
+    
     private Dictionary<string, int> variables;
 
     internal Emulator()
@@ -9,28 +11,65 @@ public class Emulator
         variables = new Dictionary<string, int>();
     }
 
-    internal void ExecuteCommand(string lhs, string operation, string rhs, string output)
+    internal void ExecuteCommand(string leftOperand, string operation, string rightOperand, string outputVariable)
     {
-        
+        int left = ParseValue(leftOperand);
+        int right = ParseValue(rightOperand);
+        ExecuteCommand(left, operation, right, outputVariable);
     }
 
-    internal void ParseMoveCommand(string lhs, string output)
+    private void ExecuteCommand(int leftOperand, string operation, int rightOperand, string outputVariable)
     {
-        
+        int result = operation switch
+        {
+            "AND" => AndOperation(leftOperand, rightOperand),
+            "OR" => OrOperation(leftOperand, rightOperand),
+            "NOT" => NotOperation(rightOperand),
+            "LSHIFT" => LShiftOperation(leftOperand, rightOperand),
+            "RSHIFT" => RShiftOperation(leftOperand, rightOperand),
+            _ => leftOperand
+        };
+
+        Store(outputVariable, result);
     }
 
-    internal void MoveCommand(int lhs, string output)
+    internal int GetVariableValue(string variableName)
     {
-        
+        return variables.GetValueOrDefault(variableName, 0);
     }
 
-    internal void AndCommand(int lhs, int rhs)
+    private void Store(string variable, int value)
     {
-        
+        variables[variable] = value % (_maxValue + 1);
     }
 
-    internal void ParseAndCommand(string lhs, string rhs)
+    private static int AndOperation(int lhs, int rhs)
     {
-        
+        return lhs & rhs;
+    }
+
+    private static int OrOperation(int lhs, int rhs)
+    {
+        return lhs | rhs;
+    }
+
+    private static int NotOperation(int value)
+    {
+        return _maxValue - value;
+    }
+
+    private static int RShiftOperation(int lhs, int rhs)
+    {
+        return lhs >> rhs;
+    }
+
+    private static int LShiftOperation(int lhs, int rhs)
+    {
+        return lhs << rhs;
+    }
+
+    private int ParseValue(string value)
+    {
+        return int.TryParse(value, out int i) ? i : variables.GetValueOrDefault(value, 0);
     }
 }
